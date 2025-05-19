b2a86faddf14c40d812561d73329ec3bbc879b9f
Sarah06102 <sarah.hra06@gmail.com>
2025-05-18 21:24:34 -0700

    edited home page layout

--- trackmyapps-extension/src/web/components/nav-bar.jsx
+++ trackmyapps-extension/src/web/components/nav-bar.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 const NavBar = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const handleHomeClick = (e) => {
+        if (location.pathname === '/') {
+          window.scrollTo({ top: 0, behavior: 'smooth' });
+        } else {
+          navigate('/');
+        }
+      };
+      
+
     return (
-        <nav className="bg-sky-700 px-4 py-3">
+        <nav className="fixed top-0 w-full z-50 bg-sky-700 px-4 py-3">
             <div className="max-w-7xl mx-auto flex items-center justify-between">
                 <div className="flex items-center space-x-6 text-white font-medium h-full">
-                    <Link className= "rounded-xl h-full px-4 py-2 hover:bg-sky-400 transition-all duration-300 ease-in-out" to='/'>Home</Link>
+                    <button onClick={handleHomeClick} className="rounded-xl h-full px-4 py-2 hover:bg-sky-400 transition-all duration-300 ease-in-out text-white">
+                        Home
+                    </button>
+                    <a className= "rounded-xl h-full px-4 py-2 hover:bg-sky-400 transition-all duration-300 ease-in-out" href="#About">About</a>
+                    <a className= "rounded-xl h-full px-4 py-2 hover:bg-sky-400 transition-all duration-300 ease-in-out" href="#Features">Features</a>
                     <Link className= "rounded-xl h-full px-4 py-2 hover:bg-sky-400 transition-all duration-300 ease-in-out" to='/login'>Login</Link>
                 </div>
             </div>
